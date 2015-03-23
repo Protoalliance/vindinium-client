431343c441ba5bd86e52fe3625660ab4c19104c4
Joseph Sankar <jesankar@ncsu.edu>
2015-03-23 16:29:14 -0400

    Added partybot

--- src/main/java/com/brianstempin/vindiniumclient/bot/advanced/partybot/AStar.java
+++ src/main/java/com/brianstempin/vindiniumclient/bot/advanced/partybot/AStar.java
@@ -1,4 +1,4 @@
-package com.brianstempin.vindiniumclient.dto;
+package com.brianstempin.vindiniumclient.bot.advanced.partybot;
 
 /**
  * Created by Joseph on 3/12/2015.
---
+++ src/main/java/com/brianstempin/vindiniumclient/bot/advanced/partybot/PartyBot.java
@@ -0,0 +1,25 @@
+package com.brianstempin.vindiniumclient.bot.advanced.partybot;
+
+import com.brianstempin.vindiniumclient.bot.BotMove;
+import com.brianstempin.vindiniumclient.bot.advanced.AdvancedBot;
+import com.brianstempin.vindiniumclient.bot.advanced.AdvancedGameState;
+
+/**
+ * Created by Joseph on 3/23/2015.
+ */
+public class PartyBot implements AdvancedBot{
+    @Override
+    public BotMove move(AdvancedGameState gameState) {
+        return null;
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
