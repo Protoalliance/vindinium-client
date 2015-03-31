67bfe7ba14c27c11653f88c24b5290606811fa85
Matthew Neal <meneal@gmail.com>
2015-03-30 22:07:04 -0400

    Working BloodthirstBot complete.  Needs a bunch of error checking and bug finding, but it works.

--- src/main/java/com/protoalliance/vindiniumclient/bot/proto/bloodthirstbot/ChaseToKillSequence.java
+++ src/main/java/com/protoalliance/vindiniumclient/bot/proto/bloodthirstbot/ChaseToKillSequence.java
@@ -1,18 +1,48 @@
 package com.protoalliance.vindiniumclient.bot.proto.bloodthirstbot;
 
 import com.protoalliance.vindiniumclient.bot.proto.BehaviorTreeBase.Blackboard;
-import com.protoalliance.vindiniumclient.bot.proto.BehaviorTreeBase.Sequence;
+import com.protoalliance.vindiniumclient.bot.proto.BehaviorTreeBase.ParentTask;
 
 /**
  * Created by Matthew on 3/29/2015.
  */
-public class ChaseToKillSequence extends Sequence {
+public class ChaseToKillSequence extends ParentTask {
 
     public ChaseToKillSequence(Blackboard bb) {
         super(bb);
         control.subTasks.add(new GetClosestBotTask(bb));
         control.subTasks.add(new PathfindToClosestBotTask(bb));
-        control.subTasks.add(new MoveToTargetTask(bb));
+        control.subTasks.add(new RunUntilFailureDecorator(bb, new MoveToTargetTask(bb)));
 
+    }
+
+    /**
+     * A child finished with failure.
+     * We failed to run the whole sequence.
+     * Bail with failure.
+     */
+    @Override
+    public void childFailed() {
+        control.finishWithFailure();
+    }
+
+    /**
+     * A child has finished with success
+     * Select the next one to update. If
+     * it's the last, we have finished with
+     * success.
+     */
+    @Override
+    public void childSucceeded() {
+        int curPos = control.subTasks.indexOf(control.currentTask);
+        if( curPos == (control.subTasks.size() - 1)) {
+            control.finishWithSuccess();
+        } else {
+            //Pre increment the pointer to the next position.
+            control.currentTask = control.subTasks.get(++curPos);
+            if(!control.currentTask.checkConditions()) {
+                control.finishWithFailure();
+            }
+        }
     }
 }
--- src/main/java/com/protoalliance/vindiniumclient/bot/proto/bloodthirstbot/RunUntilFailureDecorator.java
+++ src/main/java/com/protoalliance/vindiniumclient/bot/proto/bloodthirstbot/RunUntilFailureDecorator.java
@@ -1,9 +1,6 @@
 package com.protoalliance.vindiniumclient.bot.proto.bloodthirstbot;
 
-import com.protoalliance.vindiniumclient.bot.proto.BehaviorTreeBase.Blackboard;
-import com.protoalliance.vindiniumclient.bot.proto.BehaviorTreeBase.ParentTaskDecorator;
-import com.protoalliance.vindiniumclient.bot.proto.BehaviorTreeBase.Task;
-import com.protoalliance.vindiniumclient.bot.proto.BehaviorTreeBase.TaskDecorator;
+import com.protoalliance.vindiniumclient.bot.proto.BehaviorTreeBase.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,59 +9,105 @@ import org.apache.logging.log4j.Logger;
  * The TaskDecorator actually extends task!!!  God help us all.
  * This should actually decorate a parent task/sequence I think.
  */
-public class RunUntilFailureDecorator extends ParentTaskDecorator {
+public class RunUntilFailureDecorator extends ParentTask {
     private static final Logger logger = LogManager.getLogger(RunUntilFailureDecorator.class);
+    private Task task;
 
+    /**
+     * When instantiating RunUntilFailureDecorator
+     * objects keep in mind that the task passed in
+     * is the task that will continue running until
+     * the Decorator fails.
+     * @param bb
+     * @param task
+     */
     public RunUntilFailureDecorator(Blackboard bb, Task task) {
-        super(bb, task);
+        super(bb);
+        this.task = task;
     }
 
+    /**
+     * This is normally where we would actually return
+     * success, but we don't want to return success
+     * until there's a failure, so this is called when
+     * the child task actually fails.  When whatever
+     * is over this task calls it we will return
+     * that the task is finished and succeeded.
+     *
+     */
+    @Override
+    public void childSucceeded() {
+        control.finishWithSuccess();
+    }
+
+    /**
+     * This should never happen.  When the child fails
+     * we report success.
+     */
+    @Override
+    public void childFailed() {
+        control.finishWithFailure();
+    }
+
+    /**
+     * Ends the task
+     */
+    public void end() {
+        logger.info("Run until failure complete.");
+    }
+
+    /**
+     * This is also overridden to keep the
+     * task that is started as always the one
+     * that is passed in.
+     */
+
+    @Override
+    public void start() {
+        logger.info("Starting run until failure.");
+        control.currentTask = task;
+        control.currentTask.getController().safeStart();
+        control.currentTask.perform();
+    }
+
+    /**
+     * This the part that makes this a decorator.
+     * We override the perform method here to
+     * set up the repetition.
+     */
     @Override
     public void perform() {
         logger.info("Doing Action");
 
-        if(control.finished() && control.succeeded()) {
+        if(control.currentTask == null) {
+            // If there is a null child task
+            // selected we've done something wrong
+            logger.info("Current task was null!");
+            return;
+        }
+
+        if(control.currentTask.getController().finished()) {
             //This decorator wrecks shop here,
             //It says to reset and redo the task.
             //The sort of insane thing here is that we
             //don't want to actually call the constructor
             //again.  We just want the loop to keep running.
-            //Otherwise we just bail.
-            if(control.succeeded()) {
-                //This resets the parent task
-                control.reset();
+            //If there's a failure we actually call success!
+            if(control.currentTask.getController().succeeded()) {
                 //This resets the child task
                 control.currentTask.getController().reset();
-                control.safeStart();
+                //No need to start again, that would only
+                //reset the index!
+                //No need to reset the Decorator's control
+                //either since it never succeeded or failed.
                 return;
             }else{
-                //if we're here we failed.  bail out.
+                //If we're here we finally failed
+                //We return success!
+                childSucceeded();
                 return;
             }
-        }
-        if(control.currentTask == null) {
-            // If there is a null child task
-            // selected we've done something wrong
-            return;
-        }
-
-        // If we do have a curTask...
-        //and it's not started yet, start it.
-        if( !control.currentTask.getController().started()){
-            control.currentTask.getController().safeStart();
-        } else if(control.currentTask.getController().finished()){
-            //If we are here the task is finished.
-            //end the task
-            control.currentTask.getController().safeEnd();
-            //Check success or failure
-            if(control.currentTask.getController().succeeded()){
-                this.childSucceeded();
-            }
-            if(control.currentTask.getController().failed()){
-                this.childFailed();
-            }
-            //Task is ready, so update
-            //THIS IS WHERE THE DOING GETS DONE!
+        //If we're not done yet we should just keep performing.
         } else {
             control.currentTask.perform();
         }
